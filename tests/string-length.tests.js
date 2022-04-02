25e327c3ac65fa090d5fbf5c97f02a1ae05c23d5
isaacpitwa <isaacpitwa256@gmail.com>
2022-04-03 00:27:12 +0300

    string length  test

---
+++ tests/string-length.tests.js
@@ -0,0 +1,22 @@
+const stringLength = require("../string-length");
+
+describe('stringLength', () => {
+  it('returns a string character count', () => {
+    const string = 'myString';
+    const expected = 8;
+
+    const charCount = stringLength(string);
+
+    expect(charCount).toEqual(expected);
+  });
+
+  it('throw and error if String Length is less than 1', () => {
+    const string = '';
+    expect(() => { stringLength(string); }).toThrow();
+  });
+
+  it('throw and error if String Length is less than 1', () => {
+    const string = 'This has more than 10 characters';
+    expect(() => { stringLength(string); }).toThrow();
+  });
+});
